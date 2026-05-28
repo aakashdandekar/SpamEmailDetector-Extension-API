cddf02e177975e2c0a8f4bcdbeaba2697d1aef3f
Aakash Dandekar <aakashdandekar2006@gmail.com>
2026-05-28 15:43:09 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credintials=["*"],
+    allow_credentials=["*"],
     allow_methods=["*"],
     allow_headers=["*"]
 )
