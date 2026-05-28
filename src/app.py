7871a20732e0044f1042edd7b7971dc33537bacf
Aakash Dandekar <aakashdandekar2006@gmail.com>
2026-05-28 15:36:03 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-import pickle
+import joblib
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pathlib import Path
@@ -19,10 +19,10 @@ vectorizer_path = BASE_DIR / "assets" / "vectorizer.pkl"
 model_path = BASE_DIR / "assets" / "EmailSpamDetectionModel.pkl"
 
 with open(vectorizer_path, "rb") as f:
-    vectorizer = pickle.load(f)
+    vectorizer = joblib.load(f)
 
 with open(model_path, "rb") as f:
-    model = pickle.load(f)
+    model = joblib.load(f)
 
 @app.post('/predict')
 async def predict(
