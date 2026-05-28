9f4222c3f518f459c038e7afd8286b1dac092d87
Aakash Dandekar <aakashdandekar2006@gmail.com>
2026-05-28 16:13:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import uvicorn as uv
+import os
+import uvicorn
 
-if __name__ == '__main__':
-    uv.run("src.app:app", host="0.0.0.0", port=5000, reload=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    uvicorn.run(app, host="0.0.0.0", port=port, reload=True)
